df7c9267252343b8eee068ef237589978038abc9
Topher Brown <topher200@gmail.com>
2011-01-12 22:59:37 -0500

    have passing tests for distance-between-chars

--- src/genetic_clojure/core.clj
+++ src/genetic_clojure/core.clj
@@ -1,4 +1,5 @@
-(ns genetic-clojure.core)
+(ns genetic-clojure.core
+  (:use [clojure.contrib.math]))
 
 (def alphabet
      '("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
@@ -6,11 +7,20 @@
      "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
      "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?"))
 
+(def target
+     "Hello Clojure World!")
+
+(defn difference-between-chars
+  "Returns the distance between the int representations of two chars"
+  [a b]
+  (abs (- (int a) (int b))))
+
 (defn fitness
-  "Determines the fitness of a given chromo based on how far it is from the
-  target string"
+  "Determines the fitness of a given chromo based on how far each of its
+  characters are from the target string"
   [chromo]
-  -1)
+  (reduce + (map (fn [a b] (- (int a) (int b))) chromo target)))
+
 
 (defn breed
   "Breeds two chromos together by splitting them in a random place and
--- test/genetic_clojure/test/core.clj
+++ test/genetic_clojure/test/core.clj
@@ -3,11 +3,19 @@
   (:use [clojure.test]))
 
 (deftest fitness-test
-  (is (= 0 (fitness "Hello World!")))
-  (is (= 1 (fitness "Iello World!")))
-  (is (= 5 (fitness "Iallo World!"))))
+  (is (= "Hello Clojure World!" target))
+  (is (= 0 (fitness "Hello Clojure World!")))
+  (is (= 1 (fitness "Iello Clojure World!")))
+  (is (= 5 (fitness "Iallo Clojure World!"))))
 
 (deftest breed-test
   (is (= '("aa", "bb") (breed "aa" "bb" 0)))
   (is (= '("ab", "ab") (breed "aa" "bb" 1)))
   (is (= '("abb", "baa") (breed "aaa" "bbb" 1))))
+
+(deftest difference-between-chars-test
+  (is (= 0 (difference-between-chars \a \a)))
+  (is (= 1 (difference-between-chars \a \b)))
+  (is (= 1 (difference-between-chars \b \a)))
+  (is (= 1 (difference-between-chars \A \B)))
+  (is (= 32 (difference-between-chars \a \A))))
